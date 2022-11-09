1f9c42d1f827b3559d7de77e70256bc55d6743ad
unipuppy81 <113489927+unipuppy81@users.noreply.github.com>
2022-11-09 10:59:45 +0900

    rock life

--- Assets/02Scripts/Enemy/Rock.cs
+++ Assets/02Scripts/Enemy/Rock.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Rock : MonoBehaviour
 {
     private Player player;
-
+    public float lifetime;
     public int rockDamage;
     void Awake()
     {
@@ -17,7 +17,18 @@ public class Rock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        alive();   
+    }
+
+    void alive()
+    {
+        lifetime += Time.deltaTime;
+
+        if (lifetime >= 3.0f)
+        {
+            Destroy(gameObject);
+            lifetime = 0f;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
